df9bd5772d16fe0c1ef672db40537f571ba42ac5
group009BSE1 <2021BSE145@std.must.ac.ug>
2022-01-24 21:31:52 -0800

    Assignment 2

---
+++ src/ASSIGNMENT 2/EXERCISE3.py
@@ -0,0 +1,4 @@
+a = int(input('HOURS WORKED:'))
+b = int(input('RATE WORKED FOR:'))
+c = a * b
+print('GROSS PAY:', c)
---
+++ src/ASSIGNMENT 2/EXERCISE5.py
@@ -0,0 +1,3 @@
+a = input('DEGREES CELSIUS:')
+c = ((int(a) * 9 / 5) + 32)
+print("DEGREES FAHRENHEIT:", c)
